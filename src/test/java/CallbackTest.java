2b9797a92c4c475e92b8b8b2db19bf82ca39e8d5
Mikhail Gozhenko <sacred9900@gmail.com>
2021-10-30 19:28:41 +0500

    change test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -28,22 +28,17 @@ class CallbackTest {
     }
 
     @Test
-    public void shouldTestSomething() throws InterruptedException {
-        ChromeOptions options = new ChromeOptions(); // подскажите в комментариях правильно ли я расположил опцию headless. т.к. окно браузера запускается,  но теста не видать. Но в итоге он проходит
+    public void shouldHappyPathTest() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions(); // Подскажите в комментариях правильно ли я расположил опцию headless. т.к. окно браузера запускается,  но теста не видать. Но в итоге он проходит
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
-        sleep(1000);
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ганизада Ахад Гани-оглы");
-        sleep(1000);
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79519506677");
-        sleep(1000);
         driver.findElement(By.cssSelector(".checkbox")).click();
-        sleep(1000);
         driver.findElement(By.cssSelector(".button__text")).click();
-        sleep(1000);
         String actualMassage = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().strip();
         String expectedMassage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
