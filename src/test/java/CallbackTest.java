14d1ddacec9bd43edd16bc255a56526c6b3e81ef
Mikhail Gozhenko <sacred9900@gmail.com>
2021-11-01 16:32:57 +0500

    added tests

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -21,7 +21,7 @@ class CallbackTest {
 
     @BeforeEach
     void setupTest() {
-        ChromeOptions options = new ChromeOptions(); // Подскажите в комментариях правильно ли я расположил опцию headless. т.к. окно браузера запускается,  но теста не видать. Но в итоге он проходит
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
@@ -35,9 +35,10 @@ class CallbackTest {
     }
 
     @Test
-    public void shouldHappyPathTest() {
+    public void shouldHappyPathTest() throws InterruptedException  {
 
         driver.get("http://localhost:9999");
+        //driver.wait(2000); // оставил для примера
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ганизада Ахад Гани-оглы");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79519506677");
         driver.findElement(By.cssSelector(".checkbox")).click();
@@ -45,22 +46,84 @@ class CallbackTest {
         String actualMassage = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().strip();
         String expectedMassage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
-        driver.quit(); // дописал чтоб закрывалось окно браузера, но что-то не закрывается.
+        driver.quit(); // закрывалось окно браузера
     }
 
     @Test
-    public void shouldValidationCheckTest() {
+    public void shouldValidationFieldsNameAndSurname() { //цифры в поле имя и фамилия
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("9519506");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79519506677");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__inner'] > [class='input__sub']")).getText().strip();
+        String expectedMassage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
+        driver.quit();
+    }
+
+    @Test
+    public void shouldValidationFieldsPhone() { // Латинские буквы в поле имя и фамилия
 
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[type='text']")).sendKeys("Petr Vodkin");
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79519506677");
         driver.findElement(By.cssSelector(".checkbox")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String actualMassage = driver.findElement(By.cssSelector("[class='input__sub']")).getText().strip();
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__inner'] > [class='input__sub']")).getText().strip();
         String expectedMassage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
         driver.quit();
-
     }
+    @Test
+    public void shouldValidationFieldPhone() { //некорректный номер в поле телефон
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Григорий Дятлов");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("89519506677");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actualMassage = driver.findElement(By.cssSelector("[class*='input_invalid'] [class='input__sub']")).getText().strip();
+        String expectedMassage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
+        driver.quit();
+    }
+    @Test
+    public void shouldValidationCheckBoxTest() { //поля заполнены, а чек бокс не проставлен.
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Федор Сумкин");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79519506677");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actualMassage = driver.findElement(By.cssSelector("[class*='input_invalid'] [class='checkbox__text']")).getText().strip();
+        String expectedMassage = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
+        driver.quit();
+    }
+
+    @Test
+    public void shouldValidationEmptyFieldsAndCheckBox() { // пустые поля и чек бокс
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__inner'] > [class='input__sub']")).getText().strip();
+        String expectedMassage = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
+        driver.quit();
+    }
+
+    @Test
+    public void shouldValidationEmptyFields() { // пустые поля
+
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector(".checkbox")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actualMassage = driver.findElement(By.cssSelector("[class='input__inner'] > [class='input__sub'] ")).getText().strip();
+        String expectedMassage = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expectedMassage, actualMassage, "Сообщение не соответствует ожидаемому");
+        driver.quit();
+    }
+
 }
 
